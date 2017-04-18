6acaca547fccfa437bcf4eee61fbaa56d3030871
Taurus1998 <anton.k@phpdev.org>
2017-04-18 15:40:58 +0300

    Z_1.2 end

--- Z_1.2.php
+++ Z_1.2.php
@@ -1,10 +1,20 @@
 <?php
-$n=$_GET['n'];
+$k=$_GET['k'];
 $min=1000;
 $max=10000;
 for ($i=$min; $i<$max;$i++)
 {
-    
+    $s=0;
+    $n=$i;
+    while ($n > 0) {
+        $r = $n % 10;
+        $s += $r;
+        $n = (int)$n/10;
+    }
+    if ($s==$k)
+    {
+        echo $i.'</br>';
+    }
 
 }
 
