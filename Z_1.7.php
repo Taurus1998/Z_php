ca8b2a89ab1b4bef519930a91fb0522bd4f3ec2f
Taurus1998 <anton.k@phpdev.org>
2017-04-18 17:54:48 +0300

    Z_1.6 end

---
+++ Z_1.7.php
@@ -0,0 +1,17 @@
+<?php
+$n=$_GET['n'];
+$k=1;
+strval($n);
+for ($i=0; $i<strlen($n)-1;$i++)
+{
+    for ($j=$i+1; $j<strlen($n);$j++)
+    {
+        if($n[$i]==$n[$j] )
+        {
+            echo "Yes";
+            exit();
+        }
+    }
+}
+
+echo "No";
