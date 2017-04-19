80f4856e468a9679b1680cc97545040db7d5dc41
Taurus1998 <anton.k@phpdev.org>
2017-04-19 16:55:31 +0300

    Z_1.10 ???

--- Z_1.10.php
+++ Z_1.10.php
@@ -1,27 +1,26 @@
 <?php
 $n=$_GET['n'];
 
-for ($i=$n;$i>10;$i--)
+//for ($i=$n;$i>10;$i--)
 {
-    if(pr($i)==true )
+    if(pr($n)==true )
     {
-        echo $i."<br>";
+        echo $n."<br>";
     }
 
 }
 
-function  pr($k)
-{
 
+function pr($k)
+{
     strval($k);
 
     for ($i=0;$i<round((strlen($k)/2),0,PHP_ROUND_HALF_DOWN);$i++)
     {
 
-       // echo $k[$i]."_".$k[strlen($k)-1-$i].":";
         if ($k[$i]<>$k[strlen($k)-1-$i])
         {
-
+            echo "no</br>";
             return false;
         }
 
