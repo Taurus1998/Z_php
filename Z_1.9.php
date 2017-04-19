e3e673e711284efb7e75bf64872eac8520dfe1fa
Taurus1998 <anton.k@phpdev.org>
2017-04-19 17:36:17 +0300

    Z_1.10 end

--- Z_1.9.php
+++ Z_1.9.php
@@ -1,8 +1,8 @@
 <?php
 $n=$_GET['n'];
 
-$k=pow($n,2);
-$k=$k%pow(10,strlen($n));
+$k=k_pow($n,2);
+$k=$k%k_pow(10,strlen($n));
 if ($k==$n)
 {
     echo "Yes";
@@ -10,4 +10,15 @@ if ($k==$n)
 else
 {
     echo "No";
+}
+
+function k_pow($a,$b)
+{
+    $a1=$a;
+    for ($i=1;$i<$b;$i++)
+    {
+        $a1=$a1*$a;
+    }
+return $a1;
+
 }
