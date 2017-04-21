fa07f16361c93e344390b961380d86d0b066eb80
Taurus1998 <anton.k@phpdev.org>
2017-04-21 16:12:35 +0300

    Z_4.10 end

---
+++ Zadanie_4/Z_4.10.php
@@ -0,0 +1,56 @@
+<?php
+
+$mas = array();
+$n=3;
+$m=5;
+$max;
+for ($i = 0; $i < $n; $i++)
+{
+
+    for ($j = 0; $j < $m; $j++)
+    {
+        $mas[$i][$j] = rand(1, 9);
+        echo " ".$mas[$i][$j];
+    }
+    echo "<br>";
+}
+
+echo "<br>";
+
+
+for ($i = 0; $i < $n; $i++)
+{
+    $max=maxK($mas[$i]);
+    for ($j = 0; $j < $m; $j++)
+    {
+        if ($mas[$i][$j]==$max)
+        {
+            $mas[$i][$j]=null;
+            echo " _ ".$mas[$i][$j];
+        }
+        else
+        {
+            echo " ".$mas[$i][$j];
+        }
+
+
+    }
+    echo "<br>";
+}
+
+//print_r($mas);
+function maxK($arr)
+{
+    $max=$arr[0];
+    for($i=0;$i<count($arr);$i++)
+    {
+        if ($arr[$i]>=$max )
+        {
+            $max=$arr[$i];
+        }
+
+    }
+    return $max;
+
+}
+
